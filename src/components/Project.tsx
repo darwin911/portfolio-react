938fa6ac4b87158cea187563a067bf74bccb6e1b
Darwin Smith <darwin911@gmail.com>
2019-11-10 18:25:46 -0500

    Adds project title and project body

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Project {
   img: string;
@@ -13,24 +13,40 @@ interface Props {
 }
 
 export const Project: React.FC<Props> = ({ project }) => {
-  return (
-    <article>
-      <img className="project-pic" src={project.img} alt={project.title} />
-      <div className="project-links">
-        <a
-          className="live-site"
-          href={project.link}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Live Site
-        </a>
-        <a href={project.github} target="_blank noreferrer" rel="noopener">
-          <i className="fab fa-github project-icon-github"></i>
-        </a>
-      </div>
+  const [isHovered, setIsHovered] = useState<Boolean>(false);
 
-      <p className="img-description">{project.description}</p>
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <article className='project'>
+      <img className='project__img' src={project.img} alt={project.title} />
+
+      <div className={`project__body${isHovered ? ' triggered' : ''}`}>
+        <h3 className='project__title'>{project.title}</h3>
+        <p className='project__description'>{project.description}</p>
+        <div className='project__links'>
+          <button
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}>
+            <a
+              className='project__live-link'
+              href={project.link}
+              target='_blank'
+              rel='noopener noreferrer'>
+              Live Site
+            </a>
+          </button>
+          <a href={project.github} target='_blank noreferrer' rel='noopener'>
+            <i className='fab fa-github project__github-icon'></i>
+          </a>
+        </div>
+      </div>
     </article>
   );
 };
