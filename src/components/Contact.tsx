15b1b427cb9a86544e8c3257b216a4e4edc1bcdc
Darwin Smith <darwinpsmith@gmail.com>
2019-11-12 15:51:04 -0500

    Adds style to Contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,17 +3,24 @@ import React from 'react';
 export const Contact = () => {
   return (
     <section className="contact">
-      <h2>Contact</h2>
-      <form>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Email" />
+      <h2 className="contact__heading">Contact</h2>
+      <p className="contact__description">
+        This is here mostly to showcase the ability to style form inputs, and
+        adding the relevent functionality to a form (Send data to someone
+        through inputs)
+      </p>
+      <form className="contact__form">
+        <input type="text" placeholder="Name" className="contact__name" />
+        <input type="text" placeholder="Email" className="contact__email" />
         <textarea
           name="message"
           id="message"
           cols={30}
           rows={10}
           placeholder="Message"
+          className="contact__message"
         ></textarea>
+        <button className="contact__submit">Send!</button>
       </form>
     </section>
   );
