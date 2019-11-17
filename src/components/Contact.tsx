af4eb4fda7d48eddac3398429afa9025307031bd
Darwin Smith <darwin911@gmail.com>
2019-11-17 15:07:43 -0500

    Updates fields with Semantic Components

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,27 +1,37 @@
 import React from 'react';
-import { Form } from 'semantic-ui-react';
+import { Form, Input, Button, TextArea } from 'semantic-ui-react';
 
 export const Contact = () => {
   return (
     <section className='contact'>
-      <h2 className='contact__heading'>Contact</h2>
-      <p className='contact__description'>
-        This is here mostly to showcase the ability to style form inputs, and
-        adding the relevent functionality to a form (Send data to someone
-        through inputs)
-      </p>
-      <form className='contact__form'>
-        <input type='text' placeholder='Name' className='contact__name' />
-        <input type='text' placeholder='Email' className='contact__email' />
-        <textarea
-          name='message'
-          id='message'
-          cols={30}
-          rows={10}
-          placeholder='Message'
-          className='contact__message'></textarea>
-        <button className='contact__submit'>Send!</button>
-      </form>
+      <div className='contact__container'>
+        <h2 className='contact__heading'>Contact</h2>
+        <p className='contact__description'>
+          This is here mostly to showcase the ability to style form inputs, and
+          adding the relevent functionality to a form (Send data to someone
+          through inputs)
+        </p>
+        <Form className='contact__form'>
+          <Form.Field>
+            <Input type='text' placeholder='Name' className='contact__name' />
+          </Form.Field>
+          <Form.Field>
+            <Input type='text' placeholder='Email' className='contact__email' />
+          </Form.Field>
+          <Form.Field>
+            <TextArea
+              name='message'
+              id='message'
+              cols={30}
+              rows={10}
+              placeholder='Message'
+              className='contact__message'></TextArea>
+          </Form.Field>
+          <Button color='blue' className='contact__submit'>
+            Send!
+          </Button>
+        </Form>
+      </div>
     </section>
   );
 };
