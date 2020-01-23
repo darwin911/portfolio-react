d7b2aff5551c1e6318ba1b8a90a98c55f2958570
Darwin Smith <darwin911@gmail.com>
2020-01-23 14:37:03 -0500

    Refactor About with Material-UI Components

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,24 +3,83 @@ import profilePicture from '../assets/images/profile-pic.jpg';
 import data from '../assets/data.js';
 
 import { ListItem } from './ListItem';
+import {
+  Typography,
+  makeStyles,
+  Theme,
+  createStyles,
+  Avatar,
+  Container
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    heading: {
+      margin: theme.spacing(2)
+    },
+    subheading: {
+      margin: theme.spacing(1),
+      fontWeight: theme.typography.fontWeightBold
+    },
+    blurb: {
+      margin: theme.spacing(2)
+    },
+    avatar: {
+      margin: theme.spacing(2),
+      width: 100,
+      height: 100
+    }
+  })
+);
+
+const styles = {
+  Section: {
+    padding: '4rem 0'
+  },
+  SectionHeading: {
+    marginBottom: '1.5rem',
+    fontWeight: 700
+  },
+  Blurb: {
+    maxWidth: '80vw',
+    width: '500px',
+    marginBottom: '1.5rem'
+  }
+};
 
 export const About: React.FC = () => {
+  const classes = useStyles();
   return (
-    <section className='about'>
-      <div className='about__container'>
-        <h2 className='about__heading'>About Me</h2>
-        <article className='about__header'>
-          <div className='about__profile-picture-container'>
-            <img
-              src={profilePicture}
-              alt='Darwin Smith'
-              className='about__profile-picture'
-            />
-          </div>
+    <section
+      id='about-me'
+      className='about'
+      style={{ ...styles.Section, background: 'white' }}>
+      <Container maxWidth='sm'>
+        <Typography
+          variant='h3'
+          component='h2'
+          gutterBottom
+          style={styles.SectionHeading}>
+          About Me
+        </Typography>
+        <header className='about__header'>
+          <Avatar
+            src={profilePicture}
+            alt='Darwin Smith Avatar'
+            className={classes.avatar}
+          />
 
-          <p className='about__blurb'>{data.blurb}</p>
-        </article>
-        <h3>Skills and Tools</h3>
+          <Typography
+            variant='body1'
+            align='justify'
+            gutterBottom
+            style={styles.Blurb}>
+            {data.blurb}
+          </Typography>
+        </header>
+        <Typography variant='h5' component='h3' className={classes.subheading}>
+          Skills and Tools
+        </Typography>
         <ul className='about__skill-list'>
           {data.skills.map(item => (
             <ListItem
@@ -30,7 +89,7 @@ export const About: React.FC = () => {
             />
           ))}
         </ul>
-      </div>
+      </Container>
     </section>
   );
 };
