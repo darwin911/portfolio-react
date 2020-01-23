dc5579caeca30e66f95af5a9e8029c536c21f5ae
Darwin Smith <darwin911@gmail.com>
2020-01-23 14:39:38 -0500

    replaces css classes with inline styles

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,15 +3,25 @@ import data from '../assets/data';
 import { Project } from './Project';
 import { Typography, Container } from '@material-ui/core';
 
+const styles = {
+  Section: {
+    padding: '4rem 0'
+  },
+  SectionHeading: {
+    marginBottom: '1.5rem',
+    fontWeight: 700
+  }
+};
+
 export const Projects = () => {
   return (
-    <section id='projects' className='projects'>
-      <Typography variant='h3' component='h2'>
-        Projects
-      </Typography>
-      <Container className='projects__container'>
+    <section id='projects' style={{ ...styles.Section, background: '#FDD835' }}>
+      <Container maxWidth='md'>
+        <Typography variant='h3' component='h2' style={styles.SectionHeading}>
+          Projects
+        </Typography>
         {data.projects.map(project => (
-          <Project project={project} key={project.title} />
+          <Project key={project.title} project={project} />
         ))}
       </Container>
     </section>
