01bfc66731caf000410fd36c1fee2715b3ab9693
Darwin Smith <darwin911@gmail.com>
2020-01-23 16:11:17 -0500

    Adds footerIcon links and tooltip

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,19 +2,33 @@ import React from 'react';
 import MailIcon from '@material-ui/icons/Mail';
 import GithubIcon from '@material-ui/icons/GitHub';
 import LinkedinIcon from '@material-ui/icons/LinkedIn';
-import Divider from '@material-ui/core/Divider';
-import Typography from '@material-ui/core/Typography';
+import { Divider, Typography, Tooltip, IconButton } from '@material-ui/core';
+
+const styles = {
+  FooterIcon: {
+    color: 'black'
+  },
+  Divider: { marginTop: '1rem', marginBottom: '1rem' }
+};
 
 export const Footer: React.FC = () => {
   return (
     <footer className='footer'>
-      <div className='footer__contact-icons'>
-        <MailIcon className='footer__mail-icon' />
-        <LinkedinIcon className='footer__linkedin-icon' />
-        <GithubIcon className='footer__github-icon' />
-      </div>
-      <Divider variant='middle' light={true} />
-      <Typography>&copy; Darwin Smith {new Date().getFullYear()}</Typography>
+      <Tooltip title='darwinpsmith@gmail.com'>
+        <IconButton href='mailto:darwinpsmith@gmail.com'>
+          <MailIcon fontSize='large' style={styles.FooterIcon} />
+        </IconButton>
+      </Tooltip>
+      <IconButton href='https://linkedin.com/in/darwinpsmith' target='_blank'>
+        <LinkedinIcon fontSize='large' style={styles.FooterIcon} />
+      </IconButton>
+      <IconButton href={'https://github.com/darwin911'}>
+        <GithubIcon fontSize='large' style={styles.FooterIcon} />
+      </IconButton>
+      <Divider variant='middle' style={styles.Divider} />
+      <Typography variant='subtitle2'>
+        &copy; Darwin Smith {new Date().getFullYear()}
+      </Typography>
     </footer>
   );
 };
