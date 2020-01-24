3b0330eb40f41d2ca4eac4b86a98235bb8f592b1
Darwin Smith <darwin911@gmail.com>
2020-01-24 14:55:20 -0500

    removes unused code

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import { ListItem, ListItemText } from '@material-ui/core';
 import { styles } from './styles';
-import { Link, animateScroll as scroller } from 'react-scroll';
+import { Link } from 'react-scroll';
 
 interface Props {
   section: string;
 }
 
 export const NavItem: React.FC<Props> = ({ section }) => {
-  // const [target, setTarget] = React.useState();
-
-  const sections = document.querySelectorAll('section');
-  const handleClick = (ev: any) => {
-    ev.preventDefault();
-    console.log('handleClick', sections);
-  };
-
-  React.useEffect(() => {
-    console.log('useEffect NavITem');
-  }, []);
-
   return (
     <Link
       activeClass='active'
@@ -27,7 +15,7 @@ export const NavItem: React.FC<Props> = ({ section }) => {
       smooth={true}
       duration={500}
       offset={0}>
-      <ListItem key={section} button onClick={e => handleClick(e)}>
+      <ListItem key={section} button>
         <ListItemText primary={section} style={styles.ListItemText} />
       </ListItem>
     </Link>
