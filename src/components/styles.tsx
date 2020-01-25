0ebceb0bcce0a6bdb6fc9e4ea96ef1ab6734f451
Darwin Smith <darwin911@gmail.com>
2020-01-25 16:45:01 -0500

    Adds better shdows to subheadings, change navbar colors

--- src/components/styles.tsx
+++ src/components/styles.tsx
@@ -3,9 +3,11 @@ export const styles = {
     padding: '4rem 0'
   },
   SectionHeading: {
+    maxWidth: '100%',
     marginBottom: '2rem',
     fontWeight: 700,
-    textShadow: '1px 4px 2px rgba(0, 0, 0, 0.25)'
+    textShadow:
+      '1px 4px 2px rgba(0, 0, 0, 0.25), -2px 3px 8px rgba(0, 0, 0, 0.25)'
   },
   Blurb: {
     maxWidth: '80vw',
@@ -20,7 +22,10 @@ export const styles = {
     margin: '1rem 0'
   },
   AppBar: {
-    backgroundColor: 'rgba(255, 255, 255, 0.9)'
+    backgroundColor: 'rgba(110, 110, 110, 0.5)',
+    color: 'white',
+    boxShadow:
+      '0px 2px 2px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 2px 0px rgba(0,0,0,0.12)'
   },
   ListItemText: {
     color: 'black',
