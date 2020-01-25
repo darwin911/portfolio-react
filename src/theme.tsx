b8e165537f001fad8bfa62f9d48e7edb0fd583cc
Darwin Smith <darwin911@gmail.com>
2020-01-25 16:44:19 -0500

    Adds responsiveFontSizes

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,22 +1,24 @@
-import { createMuiTheme } from '@material-ui/core';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core';
 
-const theme = createMuiTheme({
-  typography: {
-    fontFamily: [
-      'sofia-pro',
-      'Recursive',
-      'Roboto Mono',
-      'Roboto',
-      'Helvetica Neue',
-      'Helvetica',
-      'sans-serif'
-    ].join(', ')
-  },
-  palette: {
-    background: {
-      default: '#f6f6f6'
+const theme = responsiveFontSizes(
+  createMuiTheme({
+    typography: {
+      fontFamily: [
+        'sofia-pro',
+        'Recursive',
+        'Roboto Mono',
+        'Roboto',
+        'Helvetica Neue',
+        'Helvetica',
+        'sans-serif'
+      ].join(', ')
+    },
+    palette: {
+      background: {
+        default: '#f6f6f6'
+      }
     }
-  }
-});
+  })
+);
 
 export default theme;
