657f40a359be3c6f41927e67cbcb5f0833e1b69a
Darwin Smith <darwin911@gmail.com>
2020-01-25 19:20:13 -0500

    adds hover animation to skill list item

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 interface Props {
   title: string;
@@ -6,9 +7,16 @@ interface Props {
 }
 
 export const ListItem: React.FC<Props> = ({ title, styles }) => {
+  const [isHovered, setHovered] = React.useState(false);
+
   return (
-    <li className='about__skill-item'>
+    <motion.li
+      tabIndex={0}
+      animate={{ transform: `scale(${isHovered ? 1.4 : 1})` }}
+      className='about__skill-item'
+      onMouseEnter={() => setHovered(prevVal => !prevVal)}
+      onMouseLeave={() => setHovered(prevVal => !prevVal)}>
       <i className={styles} title={title} />
-    </li>
+    </motion.li>
   );
 };
