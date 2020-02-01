e4930318ec646f79e9944bd3ba8373bca8144e06
Darwin Smith <darwin911@gmail.com>
2020-02-01 10:24:19 -0500

    converts makeStyles to inline style

--- src/components/SkillsAndTools.tsx
+++ src/components/SkillsAndTools.tsx
@@ -1,31 +1,16 @@
 import React from 'react';
-import {
-  Box,
-  Typography,
-  makeStyles,
-  Theme,
-  createStyles
-} from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import { ListItem } from './ListItem';
 import data from '../assets/data.js';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    subheading: {
-      margin: theme.spacing(1),
-      fontWeight: theme.typography.fontWeightBold
-    }
-  })
-);
-
 export const SkillsAndTools = () => {
-  const classes = useStyles();
   return (
     <Box my={6}>
       <Typography
         variant='h3'
-        className={classes.subheading}
         style={{
+          fontWeight: 'bold',
+          margin: 8,
           textShadow:
             'rgba(0, 0, 0, 0.1) 1px 4px 2px, rgba(0, 0, 0, 0.1) -2px 3px 8px'
         }}>
