d28a848b89fe3e4948b422ca5debd9766b25cfbb
Darwin Smith <darwin911@gmail.com>
2020-02-01 11:01:38 -0500

    abstracts inline styles

--- src/components/SkillsAndTools.tsx
+++ src/components/SkillsAndTools.tsx
@@ -2,18 +2,12 @@ import React from 'react';
 import { Box, Typography } from '@material-ui/core';
 import { ListItem } from './ListItem';
 import data from '../assets/data.js';
+import { styles } from './styles';
 
 export const SkillsAndTools = () => {
   return (
-    <Box my={6}>
-      <Typography
-        variant='h3'
-        style={{
-          fontWeight: 'bold',
-          margin: 8,
-          textShadow:
-            'rgba(0, 0, 0, 0.1) 1px 4px 2px, rgba(0, 0, 0, 0.1) -2px 3px 8px'
-        }}>
+    <Box mb={6}>
+      <Typography variant='h3' style={styles.AboutSubHeading}>
         Skills and Tools
       </Typography>
       <ul className='about__skill-list'>
