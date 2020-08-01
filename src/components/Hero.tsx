2673bfde6e0395007014e7166d34949999d4a1fe
Darwin Smith <darwin911@gmail.com>
2020-08-01 15:35:56 -0400

    adds fix/debounce for min-height: 100vh on mobile browsers

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,28 +3,50 @@ import { Typography } from '@material-ui/core';
 import { motion } from 'framer-motion';
 import { Countdown } from './Countdown';
 
+export function debounce(fn: Function, ms: number) {
+  let timer: any;
+  return (_: any) => {
+    clearTimeout(timer);
+    timer = setTimeout(function(_) {
+      timer = null;
+      fn.apply(_, arguments);
+    }, ms);
+  };
+}
+// https://www.pluralsight.com/guides/re-render-react-component-on-window-resize
+
 export const Hero: React.FC = () => {
   const [visible, setVisible] = React.useState<boolean>(false);
+  const [innerHeight, setInnerHeight] = React.useState<number>(window.innerHeight);
+
   const variants = {
-    visible: { opacity: 1, y: 0 },
-    hidden: { opacity: 0, y: '-25%' },
+    visible: { opacity: 1, x: 0, y: 0 },
+    hidden: { opacity: 0, x: 0, y: 20 },
   }
+
+  React.useEffect(() => {
+    const debouncedHandleResize = debounce(function handleResize() {
+      setInnerHeight(window.innerHeight);
+    }, 100);
+    window.addEventListener('resize', debouncedHandleResize);
+    return () => window.removeEventListener('resize', debouncedHandleResize);
+  });
+
   return (
-    <section className='hero'>
+    <section className='hero' style={{ height: innerHeight }}>
       <motion.article
         className='hero__text-container'
-        initial={{ opacity: 0, y: -10 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.7, delay: 2.0 }}
+        initial={{ opacity: 0, x: 0, y: -15 }}
+        animate={{ opacity: 1, x: 0, y: 0 }}
+        transition={{ ease: "easeOut", duration: 1.5, delay: 1.5 }}
         onAnimationComplete={() => setVisible(true)}>
         <Typography variant='h2' component='h1'>
           Darwin Smith
         </Typography>
         <motion.span
-          initial={{opacity: 0, y: '150%'}}
           animate={visible ? 'visible' : 'hidden'}
           variants={variants}
-          transition={{ duration: 0.5, delay: 0.5 }}>
+          transition={{ duration: 0.5, delay: 0.25 }}>
           <Typography variant='h5' component='h2'>
           Full-stack | Developer | New York City
           </Typography>
