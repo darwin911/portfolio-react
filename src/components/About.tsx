65d8306ec6c75cbbce8fb6e784c01c072ef8fb4f
Darwin Smith <darwin911@gmail.com>
2020-09-28 21:33:46 -0400

    adds donation button

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import profilePicture from '../assets/images/profilepic_white-bg.jpg';
-import data from '../assets/data.js';
-import { styles } from './styles';
-import { SkillsAndTools } from './SkillsAndTools';
+import { Container, Typography } from '@material-ui/core';
+
 import { Github } from './Github';
-import { Typography, Container } from '@material-ui/core';
+import React from 'react';
+import { SkillsAndTools } from './SkillsAndTools';
+import data from '../assets/data.js';
+import profilePicture from '../assets/images/profilepic_white-bg.jpg';
+import { styles } from './styles';
 
 export const About: React.FC = () => {
   return (
@@ -16,7 +17,8 @@ export const About: React.FC = () => {
         <div className='about__card'>
           <div className='about__card-inner-container'>
             <img src={profilePicture} alt="Darwin Smith's profile" className='about__avatar' />
-            <h6 className='about__avatar-headline'>Hi, I'm Darwin!</h6>
+            <h6 className='about__avatar-headline'>Darwin Smith</h6>
+            <DonateButton />
           </div>
 
           <p className='about__blurb'>{data.blurb}</p>
@@ -38,5 +40,23 @@ export const About: React.FC = () => {
         </Typography> */}
       </Container>
     </section>
+  );
+};
+
+const DonateButton = () => {
+  return (
+    <form action='https://www.paypal.com/cgi-bin/webscr' method='post' target='_top'>
+      <input type='hidden' name='cmd' value='_donations' />
+      <input type='hidden' name='business' value='74AG6PY82VARU' />
+      <input type='hidden' name='currency_code' value='USD' />
+      <input
+        type='image'
+        src='https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif'
+        name='submit'
+        title='PayPal - The safer, easier way to pay online!'
+        alt='Donate with PayPal button'
+      />
+      <img alt='' src='https://www.paypal.com/en_US/i/scr/pixel.gif' width='1' height='1' />
+    </form>
   );
 };
