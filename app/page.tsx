1b09357d43a6ab06adc224125a626dadc8137244
Darwin Smith <darwinpsmith@gmail.com>
2023-10-31 16:30:13 -0400

    Style update hero/about heading

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,15 @@ export default function Home() {
         height={1987}
         priority
       />
-      <div className="border-8 border-white/10 backdrop-blur-sm p-14 rounded-lg leading-none space-y-1  transition-all w-full max-w-lg">
-        <h1 className="text-3xl text-zinc-600 drop-shadow">Darwin Smith</h1>
-        <p className="drop-shadow-sm text-left text-zinc-700">
-          Software Developer | Web
-        </p>
+      <div className="max-w-7xl w-full">
+        <div className="border-8 border-white/10 backdrop-blur p-14 rounded-lg leading-none space-y-1  transition-all w-full max-w-3xl shadow-lg">
+          <h1 className="text-2xl sm:text-4xl md:text-7xl text-stone-800 drop-shadow-heading tracking-tighter font-semibold">
+            Darwin Smith
+          </h1>
+          <p className="drop-shadow-sm text-base sm:text-xl text-left opacity-50">
+            Software Developer | Web
+          </p>
+        </div>
       </div>
     </main>
   );
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -10,7 +10,7 @@ export default function MainNav() {
   return (
     <header className="relative">
       <nav className="fixed w-full bg-gray-300 p-4 mx-auto z-10">
-        <ul className="max-w-6xl mx-auto flex gap-4">
+        <ul className="max-w-7xl mx-auto flex gap-4">
           {MAIN_NAV_LINKS.map((item) => {
             const isMatchingSegment =
               (segment === null && item.label.toLowerCase() === "home") ||
