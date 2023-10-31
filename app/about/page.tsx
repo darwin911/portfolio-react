5bd923200ce6a4312153c48cf9ce3dfac4ff72f9
Darwin Smith <darwinpsmith@gmail.com>
2023-10-31 16:30:22 -0400

    update about heading

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,9 @@
 export default function AboutPage() {
-  return <h1>About</h1>;
+  return (
+    <main className="relative flex flex-col items-center justify-center p-8 sm:p-20 md:p-24 h-full">
+      <h1 className="font-bold text-7xl drop-shadow-heading tracking-tighter">
+        About
+      </h1>
+    </main>
+  );
 }
