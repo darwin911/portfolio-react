f46052ccca55cea7d9eb2c1e2cf21282d648c2d9
Darwin Smith <darwinpsmith@gmail.com>
2023-11-02 16:19:53 -0400

    update contact/projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,10 @@
 export default function ProjectsPage() {
-  return <h1>Projects</h1>;
+  return (
+    <main className="relative flex h-full flex-col items-center justify-center overflow-hidden px-4 py-8 sm:px-8 sm:py-20 md:py-24">
+      <h1 className="text-3xl font-semibold tracking-tighter text-stone-800 drop-shadow-heading dark:text-zinc-50  dark:drop-shadow-heading-light sm:text-5xl md:text-7xl lg:whitespace-nowrap">
+        Projects
+      </h1>
+      <div></div>
+    </main>
+  );
 }
