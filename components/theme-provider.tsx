05eaaf553457e415611ba42a5bf4f6616c6a44a1
Darwin Smith <darwinpsmith@gmail.com>
2023-11-02 10:22:14 -0400

    develops clientside theme provider with dynamic import

---
+++ components/theme-provider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import * as React from "react";
+
+import dynamic from "next/dynamic";
+
+import { type ThemeProviderProps } from "next-themes/dist/types";
+
+// https://github.com/vercel/next.js/issues/7322#issuecomment-1003538777
+const NextThemesProvider = dynamic(
+  () => import("next-themes").then((module) => module.ThemeProvider),
+  { ssr: false }
+);
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
