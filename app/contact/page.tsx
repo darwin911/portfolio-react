fc8973b404108bead85aca3fe8137781eca8a1bd
Darwin Smith <darwinpsmith@gmail.com>
2023-11-06 12:46:36 -0500

    fix contact mobile

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function ContactPage() {
   return (
     <main className="relative flex h-full flex-col items-center justify-center overflow-hidden px-4 py-8 sm:px-8 sm:py-20 md:p-0">
-      <div className="grid h-full w-full grid-cols-2 place-items-center">
+      <div className="grid h-full w-full place-items-center sm:grid-cols-2">
         <div>
           <h1 className="text-3xl font-semibold tracking-tighter text-stone-800 drop-shadow-heading dark:text-zinc-50  dark:drop-shadow-heading-light sm:text-5xl md:text-7xl lg:whitespace-nowrap">
             Contact
