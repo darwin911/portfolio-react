892069e7ecd0aff4e7c1681cbd8f262cfb758338
Darwin Smith <darwinpsmith@gmail.com>
2023-11-06 17:50:06 -0500

    Adds keyboard navigation to carousel

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -3,7 +3,14 @@
 import { Button } from "@/components/ui/button";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 import clsx from "clsx";
-import React, { ReactElement, ReactNode, useState } from "react";
+import React, {
+  ReactElement,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 interface CarouselProps {
   children: ReactElement | ReactElement[];
@@ -16,25 +23,61 @@ export interface CarouselChildProps {
 
 export default function Carousel({ children }: CarouselProps) {
   const [current, setCurrent] = useState<number>(0);
+  const carouselRef = useRef<HTMLDivElement | null>(null);
+
+  const previousSlide = useCallback(() => {
+    setCurrent((prev) =>
+      prev === 0 ? (children as ReactNode[]).length - 1 : current - 1
+    );
+  }, [children, current]);
+
+  const nextSlide = useCallback(() => {
+    setCurrent((prev) =>
+      prev === (children as ReactNode[]).length - 1 ? 0 : current + 1
+    );
+  }, [children, current]);
+
+  const handleKeyDown = useCallback(
+    (event: KeyboardEvent) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          // Handle left arrow key press
+          previousSlide();
+          break;
+        case "ArrowRight":
+          // Handle right arrow key press
+          nextSlide();
+          break;
+        default:
+          // Handle other key presses or do nothing
+          break;
+      }
+    },
+    [previousSlide, nextSlide]
+  );
+
+  useEffect(() => {
+    const element = carouselRef.current;
+
+    if (element) {
+      element.addEventListener("keydown", handleKeyDown);
+
+      return () => {
+        element.removeEventListener("keydown", handleKeyDown);
+      };
+    }
+  }, [handleKeyDown]);
 
   if (!Array.isArray(children) || !children.length) {
     return null;
   }
 
-  function previousSlide() {
-    setCurrent((prev) =>
-      prev === 0 ? (children as ReactNode[]).length - 1 : current - 1
-    );
-  }
-
-  function nextSlide() {
-    setCurrent((prev) =>
-      prev === (children as ReactNode[]).length - 1 ? 0 : current + 1
-    );
-  }
-
   return (
-    <div className="relative z-0 ring-zinc-800/10">
+    <div
+      className="relative z-0 ring-zinc-800/10 focus-visible:outline-none"
+      ref={carouselRef}
+      tabIndex={0}
+    >
       <div
         className={clsx(
           "ease-[cubic-bezier(0.95,0.05,0.795,0.035)] flex snap-x flex-row transition-transform duration-300"
